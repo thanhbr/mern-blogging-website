de3729fc24a8434926f42656f1b96f5a0aa7c610
ThanhMaThe <thanh.mathe@upos.vn>
2024-02-01 10:06:47 +0700

    Setting Up Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
-    return (
-        <Navbar />
-    )
+  return (
+    <Routes>
+      <Route 
+        path="/" 
+        element={<Navbar />}
+      >
+        <Route path="/sign-in" element={<h1>Sign in page</h1>} />
+        <Route path="/sign-up" element={<h1>Sign up page</h1>} />
+      </Route>
+
+    </Routes>
+  )
 }
 
 export default App;
